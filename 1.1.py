0dc5a052cb3fd7bc9a7804cd91cf3d4cd7b79008
quyle2355 <148520090+quyle2355@users.noreply.github.com>
2023-11-09 16:17:43 +0700

    thêm file bài tập

---
+++ 1.1.py
@@ -0,0 +1,27 @@
+print("**    **  ******  **      **      ******")
+print("**    **  **      **      **      **  **")
+print("********  ******  **      **      **  **")
+print("**    **  **      **      **      **  **")
+print("**    **  ******  ******  ******  ******")
+1.2
+x= float(input("Nhập giá trị x:"))
+
+y= float(input("Nhập giá trị y:"))
+
+tong = x+y
+
+hieu=x-y
+
+tich=x*y
+
+thuong=x/y
+
+print('Tổng của hai số    ', x, '+', y, '=',tong)
+
+print('Hiệu của hai số    ', x, '-', y, '=',hieu)
+
+print('Tích của hai số    ', x, '*', y, '=',tich)
+
+print('Thương của hai số  ', x, '/', y, '=',thuong)
+
+
